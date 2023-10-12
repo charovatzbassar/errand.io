14d5df555224814f70fc7aba0d3c038389b61ec9
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-10-12 23:27:29 +0200

    init dependencies

--- api/index.js
+++ api/index.js
@@ -5,9 +5,30 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const app = express();
+const port = process.env.API_PORT;
 
-app.listen(process.env.API_PORT, () =>
-  console.log("Listening on port " + process.env.API_PORT)
+mongoose
+  .connect(process.env.DEV_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database: " + error);
+  });
+
+app.use(
+  cors({
+    origin: "http://localhost:" + process.env.APP_PORT,
+    credentials: true,
+  })
 );
+app.use(mongoSanitize());
+
+
+app.listen(port, () => console.log("Listening on port " + port));
