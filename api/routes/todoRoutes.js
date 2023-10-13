eb8c334801a41685da095106a4491ee92ac8b7fa
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-10-13 18:07:06 +0200

    api routes

--- api/routes/todoRoutes.js
+++ api/routes/todoRoutes.js
@@ -9,13 +9,61 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    
+  const newTodoGroup = new TodoGroup({ title: req.body.title });
+  await newTodoGroup.save();
+  res.json(newTodoGroup);
+});
+
+router.put("/:groupId", async (req, res) => {
+  const { groupId } = req.params;
+
+  const todoGroup = await TodoGroup.findByIdAndUpdate(groupId, {
+    title: req.body.title,
+  });
+
+  res.json(todoGroup);
+});
+
+router.delete("/:groupId", async (req, res) => {
+  const { groupId } = req.params;
+  await Todo.deleteMany({ todoGroup: groupId });
+  await TodoGroup.findByIdAndDelete(groupId);
+  res.redirect("/todos");
 });
 
 router.get("/:groupId", async (req, res) => {
   const { groupId } = req.params;
   const todos = await Todo.find({ todoGroup: groupId });
   res.json(todos);
+});
+
+router.post("/:groupId", async (req, res) => {
+  const { groupId } = req.params;
+  const newTodo = new Todo({
+    ...req.body,
+    date: new Date(),
+    completed: false,
+    todoGroup: groupId,
+  });
+
+  await newTodo.save();
+
+  res.json(newTodo);
+});
+
+router.put("/:groupId/:todoId", async (req, res) => {
+  const { groupId, todoId } = req.params;
+  const todo = await Todo.findOneAndUpdate(
+    { _id: todoId, todoGroup: groupId },
+    { ...req.body, date: new Date() }
+  );
+  res.json(todo);
+});
+
+router.delete("/:groupId/:todoId", async (req, res) => {
+  const { groupId, todoId } = req.params;
+  const todo = await Todo.findOneAndDelete({ _id: todoId, todoGroup: groupId });
+  res.json(todo);
 });
 
 router.get("/:groupId/:todoId", async (req, res) => {
