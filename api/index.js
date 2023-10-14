36bc272295f8f7805512d387a5e7a5f53d01595e
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-10-14 13:13:53 +0200

    add api error handling

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const mongoSanitize = require("express-mongo-sanitize");
 const todoRoutes = require("./routes/todoRoutes");
+const ExpressError = require("./utils/ExpressError");
 
 const app = express();
 const port = process.env.API_PORT;
@@ -34,5 +35,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use("/todos", todoRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Something went wrong :/";
+  res.status(status).json({ message: err.message, status });
+});
 
 app.listen(port, () => console.log("Listening on port " + port));
--- api/routes/todoRoutes.js
+++ api/routes/todoRoutes.js
@@ -6,74 +6,92 @@ const { catchAsync } = require("../utils/catchAsync");
 
 router
   .route("/")
-  .get(async (req, res) => {
-    const todoGroups = await TodoGroup.find({});
-    res.json(todoGroups);
-  })
-  .post(async (req, res) => {
-    const newTodoGroup = new TodoGroup({ title: req.body.title });
-    await newTodoGroup.save();
-    res.json(newTodoGroup);
-  });
+  .get(
+    catchAsync(async (req, res) => {
+      const todoGroups = await TodoGroup.find({});
+      res.json(todoGroups);
+    })
+  )
+  .post(
+    catchAsync(async (req, res) => {
+      const newTodoGroup = new TodoGroup({ title: req.body.title });
+      await newTodoGroup.save();
+      res.json(newTodoGroup);
+    })
+  );
 
 router
   .route("/:groupId")
-  .put(async (req, res) => {
-    const { groupId } = req.params;
+  .put(
+    catchAsync(async (req, res) => {
+      const { groupId } = req.params;
 
-    const todoGroup = await TodoGroup.findByIdAndUpdate(groupId, {
-      title: req.body.title,
-    });
+      const todoGroup = await TodoGroup.findByIdAndUpdate(groupId, {
+        title: req.body.title,
+      });
 
-    res.json(todoGroup);
-  })
-  .delete(async (req, res) => {
-    const { groupId } = req.params;
-    await Todo.deleteMany({ todoGroup: groupId });
-    await TodoGroup.findByIdAndDelete(groupId);
-    res.redirect("/todos");
-  })
-  .get(async (req, res) => {
-    const { groupId } = req.params;
-    const todos = await Todo.find({ todoGroup: groupId });
-    res.json(todos);
-  })
-  .post(async (req, res) => {
-    const { groupId } = req.params;
-    const newTodo = new Todo({
-      ...req.body,
-      date: new Date(),
-      completed: false,
-      todoGroup: groupId,
-    });
+      res.json(todoGroup);
+    })
+  )
+  .delete(
+    catchAsync(async (req, res) => {
+      const { groupId } = req.params;
+      await Todo.deleteMany({ todoGroup: groupId });
+      await TodoGroup.findByIdAndDelete(groupId);
+      res.redirect("/todos");
+    })
+  )
+  .get(
+    catchAsync(async (req, res) => {
+      const { groupId } = req.params;
+      const todos = await Todo.find({ todoGroup: groupId });
+      res.json(todos);
+    })
+  )
+  .post(
+    catchAsync(async (req, res) => {
+      const { groupId } = req.params;
+      const newTodo = new Todo({
+        ...req.body,
+        date: new Date(),
+        completed: false,
+        todoGroup: groupId,
+      });
 
-    await newTodo.save();
+      await newTodo.save();
 
-    res.json(newTodo);
-  });
+      res.json(newTodo);
+    })
+  );
 
 router
   .route("/:groupId/:todoId")
-  .put(async (req, res) => {
-    const { groupId, todoId } = req.params;
-    const todo = await Todo.findOneAndUpdate(
-      { _id: todoId, todoGroup: groupId },
-      { ...req.body, date: new Date() }
-    );
-    res.json(todo);
-  })
-  .delete(async (req, res) => {
-    const { groupId, todoId } = req.params;
-    const todo = await Todo.findOneAndDelete({
-      _id: todoId,
-      todoGroup: groupId,
-    });
-    res.json(todo);
-  })
-  .get(async (req, res) => {
-    const { groupId, todoId } = req.params;
-    const todo = await Todo.find({ _id: todoId, todoGroup: groupId });
-    res.json(todo);
-  });
+  .put(
+    catchAsync(async (req, res) => {
+      const { groupId, todoId } = req.params;
+      const todo = await Todo.findOneAndUpdate(
+        { _id: todoId, todoGroup: groupId },
+        { ...req.body, date: new Date() }
+      );
+      res.json(todo);
+    })
+  )
+  .delete(
+    catchAsync(async (req, res) => {
+      const { groupId, todoId } = req.params;
+      const todo = await Todo.findOneAndDelete({
+        _id: todoId,
+        todoGroup: groupId,
+      });
+      res.json(todo);
+    })
+  )
+  .get(
+    catchAsync(async (req, res) => {
+      const { groupId, todoId } = req.params;
+      const todo = await Todo.find({ _id: todoId, todoGroup: groupId });
+      res.json(todo);
+    })
+  );
 
 module.exports = router;
